4a6332150eff21f7d842063257171068f6f5f2ad
MarniaTu <147163020+MarniaTu@users.noreply.github.com>
2024-06-21 10:45:00 +0300

    change slowMO from 15000 to 2000 ms in config file

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,7 +3,7 @@ module.exports = {
         headless: false,
         defaultViewport: null,
         args: ['--start-maximized'], //— используем максимальный размер окна браузера
-        slowMo: 15000
+        slowMo: 2000
       },
     
   };
